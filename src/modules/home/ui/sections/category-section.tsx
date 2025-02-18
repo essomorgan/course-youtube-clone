551e6de666315810e57541fb7297c875625dc66b
morgan <essolomorgan@gmail.com>
2025-02-18 15:50:37 +0800

    Video categories - load data and show on layout

--- src/modules/home/ui/sections/category-section.tsx
+++ src/modules/home/ui/sections/category-section.tsx
@@ -1,13 +1,29 @@
 'use client';
 
 import { trpc } from '@/trpc/client';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Suspense } from 'react';
+import { FilterCarousel } from '@/components/filter-carousel';
 
 interface CategoriesSectionProps {
 	categoryId?: string;
 }
 
-export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
 	const [categories] = trpc.categories.getMany.useSuspenseQuery();
+	const data = categories.map(({ name, id }) => ({
+		value: id,
+		label: name,
+	}));
+	return <FilterCarousel value={categoryId} data={data} />;
+};
 
-	return <div>{JSON.stringify(categories)}</div>;
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+	return (
+		<Suspense fallback={<p>Loading...</p>}>
+			<ErrorBoundary fallback={<p>Error...</p>}>
+				<CategoriesSectionSuspense categoryId={categoryId} />
+			</ErrorBoundary>
+		</Suspense>
+	);
 };
