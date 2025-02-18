d72c86f010240c1c77acb98fab3b526ed36d2841
morgan <essolomorgan@gmail.com>
2025-02-18 10:29:54 +0800

    Video categories - organize tRPC routers

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -3,7 +3,7 @@
 import { trpc } from '@/trpc/client';
 
 export const PageClient = () => {
-	const [data] = trpc.hello.useSuspenseQuery({ text: 'Antomio' });
+	const [data] = trpc.categories.getMany.useSuspenseQuery();
 
-	return <div>Page client says: {data.greeting}</div>;
+	return <div>{JSON.stringify(data)}</div>;
 };
