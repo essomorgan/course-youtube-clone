02e7b951ca7752fdd39f4b4973ec251d72d2758d
morgan <essolomorgan@gmail.com>
2025-02-18 10:22:30 +0800

    Video categories - set table & insert data

---
+++ src/scripts/seed-categories.ts
@@ -0,0 +1,39 @@
+//TBD: create a script to seed categories
+
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames = [
+  "Cars and vehicles",
+  "Comedy",
+  "Education",
+  "Gameing",
+  "Entertainment",
+  "Flim and animation",
+  "How-to and style",
+  "Music",
+  "News and politics",
+  "People and animals",
+  "Science and technology",
+  "Sports",
+  "Travel and events",
+];
+
+async function main(/* params:type */) {
+
+
+  try {
+    const values = categoryNames.map((name) => ({
+      name,
+      description: `Videos related to ${name.toLowerCase()}`,
+    }));
+
+    await db.insert(categories).values(values);
+    console.log("Categories seeded successfully!");
+  } catch (error) {
+    console.error("Error seeding categories:", error);
+    process.exit(1);
+  }
+};
+
+main();
