53fd42955543777da42360cc746618c3bd55ac92
morgan <essolomorgan@gmail.com>
2025-03-12 19:57:19 +0800

    Studio videos - add category relation

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -11,6 +11,10 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
 
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos)
+}));
+
 export const categories = pgTable("categories", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
@@ -19,11 +23,16 @@ export const categories = pgTable("categories", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("name_idx").on(t.name)]);
 
+export const categoryRelations = relations(categories, ({ many }) => ({
+  videos: many(videos)
+}));
+
 export const videos = pgTable("videos", {
   id: uuid("id").primaryKey().defaultRandom(),
   title: text("title").notNull(),
   description: text("description"),
   userId: uuid("user_id").references(() => users.id, { onDelete: 'cascade' }).notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, { onDelete: 'set null' }),
   createAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
@@ -32,5 +41,9 @@ export const videoRelations = relations(videos, ({ one }) => ({
   user: one(users, {
     fields: [videos.userId],
     references: [users.id],
-  })
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
 }));
