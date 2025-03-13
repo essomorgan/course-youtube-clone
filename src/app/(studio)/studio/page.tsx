1b32a26988321f294428b8d0ce5c749b4c089529
morgan <essolomorgan@gmail.com>
2025-03-13 08:37:21 +0800

    Studio videos - create procedure and show json on the page

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,5 +1,15 @@
-const Page = () => {
-	return <div>Studio</div>;
+import { DEFAULT_LIMIT } from '@/constants';
+import { StudioView } from '@/modules/studio/ui/view/studio-view';
+import { trpc } from '@/trpc/server';
+
+const Page = async () => {
+	void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+
+	return (
+		<div>
+			<StudioView />
+		</div>
+	);
 };
 
 export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { studioRouter } from '@/modules/studio/server/procedures';
 import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
+  studio: studioRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
