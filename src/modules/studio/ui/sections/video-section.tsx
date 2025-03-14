0e808ff6fe9d6cc33b6c7ce3cafb306d74993609
morgan <essolomorgan@gmail.com>
2025-03-14 12:58:48 +0800

    Studio videos - add video creation

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -4,6 +4,7 @@ import { DEFAULT_LIMIT } from '@/constants';
 import { trpc } from '@/trpc/client';
 
 export const VideosSection = () => {
+	//if don't want to prefetch, just use useQuery.
 	const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
 		{
 			limit: DEFAULT_LIMIT,
