b6332e251ff66691d329ace7067a7196b1d31f13
morgan <essolomorgan@gmail.com>
2025-03-14 14:28:35 +0800

    Infinite loading - create content table

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { InfiniteScroll } from '@/components/infinite-scroll';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { DEFAULT_LIMIT } from '@/constants';
 import { trpc } from '@/trpc/client';
+import Link from 'next/link';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
@@ -18,7 +20,7 @@ export const VideosSection = () => {
 
 export const VideosSectionSuspense = () => {
 	//if don't want to prefetch, just use useQuery.
-	const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+	const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
 		{
 			limit: DEFAULT_LIMIT,
 		},
@@ -28,7 +30,38 @@ export const VideosSectionSuspense = () => {
 	);
 	return (
 		<div>
-			{JSON.stringify(data)}
+			<div className='border-y'>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className='pl-6 w-[510px]'>Video</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className='text-right'>Views</TableHead>
+							<TableHead className='text-right'>Comments</TableHead>
+							<TableHead className='text-right pr-6'>Likes</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{videos.pages
+							.flatMap((page) => page.items)
+							.map((video) => (
+								<Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+									<TableRow className='cursor-pointer'>
+										<TableCell>{video.title}</TableCell>
+										<TableCell>visibility</TableCell>
+										<TableCell>status</TableCell>
+										<TableCell>date</TableCell>
+										<TableCell>views</TableCell>
+										<TableCell>comments</TableCell>
+										<TableCell>like</TableCell>
+									</TableRow>
+								</Link>
+							))}
+					</TableBody>
+				</Table>
+			</div>
 			<InfiniteScroll isManual hasNextPage={query.hasNextPage} isFetchingNextPage={query.isFetchingNextPage} fetchNextPage={query.fetchNextPage} />
 		</div>
 	);
