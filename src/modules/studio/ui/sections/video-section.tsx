d0ffe7d4d498a66f4facb95c029ab3cf272ec0dc
morgan <essolomorgan@gmail.com>
2025-03-19 09:53:00 +0800

    Mux webhooks - import status & date

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -3,8 +3,10 @@
 import { InfiniteScroll } from '@/components/infinite-scroll';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { DEFAULT_LIMIT } from '@/constants';
+import { snakeCaseToTitle } from '@/lib/utils';
 import { VideoThumbnail } from '@/modules/videos/ui/components/video-thumbnail';
 import { trpc } from '@/trpc/client';
+import { format } from 'date-fns';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -60,11 +62,17 @@ export const VideosSectionSuspense = () => {
 														duration={video.duration || 0}
 													/>
 												</div>
+												<div className='flex flex-col overflow-hidden gap-y-1'>
+													<span className='text-sm line-clamp-1'>{video.title}</span>
+													<span className='text-xs text-muted-foreground line-clamp-1'>{video.description || 'No description'}</span>
+												</div>
 											</div>
 										</TableCell>
 										<TableCell>visibility</TableCell>
-										<TableCell>status</TableCell>
-										<TableCell>date</TableCell>
+										<TableCell>
+											<div className='flex items-center'>{snakeCaseToTitle(video.muxStatus || 'error')}</div>
+										</TableCell>
+										<TableCell className='text-sm truncate'>{format(new Date(video.createAt), 'yyyy/MM/dd')}</TableCell>
 										<TableCell>views</TableCell>
 										<TableCell>comments</TableCell>
 										<TableCell>like</TableCell>
