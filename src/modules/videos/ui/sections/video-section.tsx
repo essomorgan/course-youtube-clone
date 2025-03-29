38e4b5915779ae59c5594df290b8521afb8fca0e
morgan <essolomorgan@gmail.com>
2025-03-29 11:17:33 +0800

    Video Page - add video banner

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { trpc } from '@/trpc/client';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { VideoPlayer } from '../components/video-player';
+import { VideoBanner } from '../components/video-banner';
 
 interface VideoSectionProps {
 	videoId: string;
@@ -21,5 +24,12 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
 	const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
 
-	return <div>{JSON.stringify(video)}</div>;
+	return (
+		<>
+			<div className={cn('aspect-video bg-black rounded-xl overflow-hidden relative', video.muxStatus !== 'ready' && 'rounded-b-none')}>
+				<VideoPlayer autoPlay onPlay={() => {}} playbackId={video.muxPlaybackId} thumbnailUrl={video.thumbnailUrl} />
+			</div>
+			<VideoBanner status={video.muxStatus} />
+		</>
+	);
 };
