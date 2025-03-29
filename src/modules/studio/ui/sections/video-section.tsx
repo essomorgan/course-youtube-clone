22d317e2cc55d8bb0537221ec118e3d62b7b6b61
morgan <essolomorgan@gmail.com>
2025-03-29 10:55:08 +0800

    Video Form - fix hydration error

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -27,50 +27,52 @@ export const VideoSectionSkeleton = () => {
 	return (
 		<>
 			<div className='border-y'>
-				<TableHeader>
-					<TableRow>
-						<TableHead className='pl-6 w-[510px]'>Video</TableHead>
-						<TableHead>Visibility</TableHead>
-						<TableHead>Status</TableHead>
-						<TableHead>Date</TableHead>
-						<TableHead className='text-right'>Views</TableHead>
-						<TableHead className='text-right'>Comments</TableHead>
-						<TableHead className='text-right pr-6'>Likes</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{Array.from({ length: 1 }).map((_, index) => (
-						<TableRow key={index}>
-							<TableCell className='pl-6'>
-								<div className='flex items-center gap-4'>
-									<Skeleton className='h-20 w-36' />
-									<div className='flex flex-col gap-2'>
-										<Skeleton className='h-4 w-[100px]' />
-										<Skeleton className='h-3 w-[150px]' />
-									</div>
-								</div>
-							</TableCell>
-							<TableCell>
-								<Skeleton className='h-4 w-20' />
-							</TableCell>
-							<TableCell>
-								<Skeleton className='h-4 w-16' />
-							</TableCell>
-							<TableCell>
-								<Skeleton className='h-4 w-24' />
-							</TableCell>
-							<TableCell className='text-right'>
-								<Skeleton className='h-4 w-12 ml-auto' />
-							</TableCell>
-							<TableCell className='text-right'>
-								<Skeleton className='h-4 w-12 ml-auto' />
-							</TableCell>
-							<TableCell className='text-right pr-6'>
-								<Skeleton className='h-4 w-12 ml-auto' />
-							</TableCell>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className='pl-6 w-[510px]'>Video</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className='text-right'>Views</TableHead>
+							<TableHead className='text-right'>Comments</TableHead>
+							<TableHead className='text-right pr-6'>Likes</TableHead>
 						</TableRow>
-					))}
-				</TableBody>
+					</TableHeader>
+					<TableBody>
+						{Array.from({ length: 1 }).map((_, index) => (
+							<TableRow key={index}>
+								<TableCell className='pl-6'>
+									<div className='flex items-center gap-4'>
+										<Skeleton className='h-20 w-36' />
+										<div className='flex flex-col gap-2'>
+											<Skeleton className='h-4 w-[100px]' />
+											<Skeleton className='h-3 w-[150px]' />
+										</div>
+									</div>
+								</TableCell>
+								<TableCell>
+									<Skeleton className='h-4 w-20' />
+								</TableCell>
+								<TableCell>
+									<Skeleton className='h-4 w-16' />
+								</TableCell>
+								<TableCell>
+									<Skeleton className='h-4 w-24' />
+								</TableCell>
+								<TableCell className='text-right'>
+									<Skeleton className='h-4 w-12 ml-auto' />
+								</TableCell>
+								<TableCell className='text-right'>
+									<Skeleton className='h-4 w-12 ml-auto' />
+								</TableCell>
+								<TableCell className='text-right pr-6'>
+									<Skeleton className='h-4 w-12 ml-auto' />
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
 			</div>
 		</>
 	);
@@ -84,7 +86,7 @@ export const VideosSectionSuspense = () => {
 		},
 		{
 			getNextPageParam: (lastPage) => lastPage.nextCursor,
-		}
+		},
 	);
 	return (
 		<div>
