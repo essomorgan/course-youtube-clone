40a429b2722e225617b5799049c3e1595db594f6
morgan <essolomorgan@gmail.com>
2025-03-29 18:43:51 +0800

    Video page - add video reaction & video menu component

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,5 +1,7 @@
 import { VideoGetOneOutput } from '../../typs';
+import { VideoMenu } from './video-menu';
 import { VideoOwner } from './video-owner';
+import { VideoReactions } from './video-reactions';
 
 interface VideoTopRowProps {
 	video: VideoGetOneOutput;
@@ -11,6 +13,10 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
 			<h1 className='text-xl font-semibold'>{video.title}</h1>
 			<div className='flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4'>
 				<VideoOwner user={video.user} videoId={video.id} />
+				<div className='flex overflow-x-auto sm:min-w-[clac(50%-6px)] sm:justify-end sm:overflow-visible pb-2 sm:pb-0 sm:mb-0 gap-2'>
+					<VideoReactions />
+					<VideoMenu />
+				</div>
 			</div>
 		</div>
 	);
