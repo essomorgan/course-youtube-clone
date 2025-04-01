ee635da159dbdfad4c34c55507b81e3e0fd5b04d
morgan <essolomorgan@gmail.com>
2025-04-01 09:28:26 +0800

    Subscriptions - set schema & api

--- src/modules/video-reactions/server/procedure.ts
+++ src/modules/video-reactions/server/procedure.ts
@@ -85,4 +85,4 @@ export const videoReactionsRouter = createTRPCRouter({
 
 			return createdVideoReactionDislike;
 		}),
-})
+});
--- src/modules/video-views/server/procedures.ts
+++ src/modules/video-views/server/procedures.ts
@@ -28,4 +28,4 @@ export const videoViewsRouter = createTRPCRouter({
 
 			return createdVideoView;
 		}),
-})
+});
