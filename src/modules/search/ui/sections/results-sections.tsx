9fcc94febec1ad60f2fcb9470eeeedfeac338fc8
morgan <essolomorgan@gmail.com>
2025-04-05 18:39:42 +0800

    Improvements - add result skeleton

--- src/modules/search/ui/sections/results-sections.tsx
+++ src/modules/search/ui/sections/results-sections.tsx
@@ -1,17 +1,47 @@
 'use client';
 
+import { InfiniteScroll } from '@/components/infinite-scroll';
 import { DEFAULT_LIMIT } from '@/constants';
 import { useIsMobile } from '@/hooks/use-mobile';
-import { VideoGridCard } from '@/modules/videos/ui/components/video-grid-card';
-import { VideoRowCard } from '@/modules/videos/ui/components/video-row-card';
+import { VideoGridCard, VideoGridCardSkeleton } from '@/modules/videos/ui/components/video-grid-card';
+import { VideoRowCard, VideoRowCardSkeleton } from '@/modules/videos/ui/components/video-row-card';
 import { trpc } from '@/trpc/client';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 interface ResultsSctionProps {
 	query: string | undefined;
 	categoryId: string | undefined;
 }
 
-export const ResultsSection = ({ query, categoryId }: ResultsSctionProps) => {
+export const ResultSection = (props: ResultsSctionProps) => {
+	return (
+		<Suspense fallback={<ResultsSctionsSkeleton />}>
+			<ErrorBoundary fallback={<p>Error...</p>}>
+				<ResultsSectionSuspense {...props} />
+			</ErrorBoundary>
+		</Suspense>
+	);
+};
+
+const ResultsSctionsSkeleton = () => {
+	return (
+		<div>
+			<div className='hidden flex-col gap-4 md:flex'>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<VideoRowCardSkeleton key={index} />
+				))}
+			</div>
+			<div className='flex flex-col gap-4 gap-y-10 pt-6 md:hidden'>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<VideoGridCardSkeleton key={index} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export const ResultsSectionSuspense = ({ query, categoryId }: ResultsSctionProps) => {
 	const isMobile = useIsMobile();
 	const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
 		{ query, categoryId, limit: DEFAULT_LIMIT },
@@ -39,6 +69,11 @@ export const ResultsSection = ({ query, categoryId }: ResultsSctionProps) => {
 						))}
 				</div>
 			)}
+			<InfiniteScroll
+				hasNextPage={resultQuery.hasNextPage}
+				isFetchingNextPage={resultQuery.isFetchingNextPage}
+				fetchNextPage={resultQuery.fetchNextPage}
+			/>
 		</>
 	);
 };
